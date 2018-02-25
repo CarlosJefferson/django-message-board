aa46c81f48ec116954ac25905a1f2025ec7e9bf8
Carlos Jefferson (JS) <carlos.jefferson@gmail.com>
2018-02-24 21:12:10 -0300

    New updates for Heroku deployment

--- messageboard_project/settings.py
+++ messageboard_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dw@dm-ba%186y&#4i4v#wt(227nqfqj#4&*hk+o4$azc*cqt8h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
